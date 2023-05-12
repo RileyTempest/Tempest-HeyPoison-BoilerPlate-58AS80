346144af8f7c2f9a71bb2b8224a46bd4eb9aaef8
Tempest <rileytempest@gmail.com>
2023-05-12 11:49:52 -0400

    the branch will break here.

---
+++ Assets/Tempest/tayXx/Nexx.cs
@@ -0,0 +1,19 @@
+namespace Tempest.tayXx
+{
+    public class Nexx
+    {
+        //Hail Buer!
+        //For the interrupter
+        //Keeps me from details
+        //This figment, imbeded
+        //Will turn back round
+        //It was too good to miss
+        
+        //Hail Nexx!
+        //Bespoke
+        //Speaks when needed. 
+        //Words chosen on azimuth
+        //Careful plots
+        //Wisely seeded. 
+    }
+}
